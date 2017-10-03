0e1977e3f82c21cfacd8d3ff8ce1c6b5ec3c96c2
softeyes <jaredko10@gmail.com>
2017-10-03 22:26:29 +0800

    first commit for phaser_test

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+	game.load.image('sky', 'images/sky.png');
+    game.load.image('ground', 'images/platform.png');
+    game.load.image('star', 'images/star.png');
+    game.load.spritesheet('dude', 'images/dude.png', 32, 48);
+}
+
+function create() {
+	game.add.sprite(0, 0, 'star');
+}
+
+function update() {
+}
